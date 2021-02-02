88632c13e5109f1229243309aa1968d54e0dfb49
Edoardo Arnaudo <edoardo.arn@gmail.com>
2021-02-02 16:06:15 +0000

    add gitignore, requirements, modified examples

--- APIv2/Python/fc_examples.py
+++ APIv2/Python/fc_examples.py
@@ -1,23 +1,41 @@
+import os
 import fc_api
 import pprint
+import json
+from datetime import datetime
+from dotenv import load_dotenv
+from urllib import request
+from urllib.error import HTTPError
+from tqdm import tqdm
 
-uri = 'https://api.fieldclimate.com/v2'
 
-#take this keys from https://ng.fieldclimate.com > User menu > API services > GENERATE NEW
-publicKey = 'copy your public key'
-privateKey = 'copy your private key'
+if __name__ == "__main__":
+    load_dotenv()
+    uri = 'https://api.fieldclimate.com/v2'
 
-station_id = '00000146'     # an example demo station
+    #take this keys from https://ng.fieldclimate.com > User menu > API services > GENERATE NEW
+    publicKey = os.getenv("METOS_PUBLIC_KEY")
+    privateKey = os.getenv("METOS_PRIVATE_KEY")
+    station_id = os.getenv("METOS_STATION_ID")     # an example demo station
+    destination = os.getenv("METOS_DATA_DIR")
 
-api = fc_api.FcApi(uri, publicKey, privateKey)
+    api = fc_api.FcApi(uri, publicKey, privateKey)
 
-pprint.pprint(api.get('/user/stations').json())
+    dates = api.get(f'/camera/{station_id}/photos/info').json()
+    pprint.pprint(dates)
 
-pprint.pprint(api.get('/station/'+station_id).json())
-pprint.pprint(api.get('/data/'+station_id+'/raw/from/1574985549/to/1574988303').json())
-pprint.pprint(api.get('/data/'+station_id+'/raw/last/24h').json()['data'])
-pprint.pprint(api.get('/data/'+station_id+'/hourly/last/2d').json()['data'])
-pprint.pprint(api.get('/data/'+station_id+'/daily/last/7d').json()['data'])
-
-pprint.pprint(api.get('/chart/highchart/'+station_id+'/hourly/last/1d').json())
-pprint.pprint(api.get('/chart/image/'+station_id+'/raw/last/16').json())
+    first = int(datetime.strptime(dates["first"], "%Y-%m-%d %H:%M:%S").timestamp())
+    last  = int(datetime.strptime(dates["last"], "%Y-%m-%d %H:%M:%S").timestamp())
+    photos = api.get(f"/camera/{station_id}/photos/from/{first}/to/{last}/0").json()
+    # store metadata (image info, bboxes, urls...)
+    with open(os.path.join(destination, "data.json"), "w", encoding="utf-8") as f:
+        json.dump(photos, f, indent=4)
+    # store each image
+    for data in tqdm(photos):
+        url = None
+        try:
+            url = data["url"]
+            name = url.split("/")[-1]
+            request.urlretrieve(url, os.path.join(destination, "images", name))
+        except HTTPError:
+            print(f"{url} not found")
